0bcf15068e3a9fa0acb897608d60a15f190aff39
Nitin Raina <94776214+nitin3000ny@users.noreply.github.com>
2023-11-13 14:45:08 +0530

    Update App.jsx
    added index to path of the shop

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 			<BrowserRouter >
 			<Navbar />
 			<Routes>
-				<Route path="/" element={<Shop/>} />
+				<Route index element={<Shop/>} />
 				<Route path="/mens" element={<ShopCategory banner={"https://images.unsplash.com/photo-1490114538077-0a7f8cb49891?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWVucyUyMGJhbm5lcnxlbnwwfHwwfHx8MA%3D%3D"} category="men" />} />
 				<Route path="/womens" element={<ShopCategory banner={"https://plus.unsplash.com/premium_photo-1661724321823-0b709f89b7d5?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d29tZW4lMjBiYW5uZXJ8ZW58MHx8MHx8fDA%3D"} category="women" />} />
 				<Route path="/kids" element={<ShopCategory banner={"https://images.unsplash.com/photo-1485309086598-f9d010ad3972?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8a2lkcyUyMGJhbm5lcnxlbnwwfHwwfHx8MA%3D%3D"} category="kids" />} />
